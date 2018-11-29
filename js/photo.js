307011fee106d0cd17f73f9864e7bfd30e509a40
rafeeq DU TOIT <rdu-toi@c3r3s4.wethinkcode.co.za>
2018-11-29 14:02:25 +0200

    done with webcam. must store images to database.

--- js/photo.js
+++ js/photo.js
@@ -1,20 +1,16 @@
 (function() {
-    var video = document.getElementById('video'),
-    vendorUrl = window.URL || window.webkitURL;
-
-    navigator.getMedia =    (navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia ||
-                            navigator.msGetUserMedia);
-
-    navigator.getMedia({
-        video: true,
-        audio: false
-    }, function(stream) {
-        video.src = vendorUrl.createObjectURL(stream);
-        video.play();
-    }, function(error) {
-        // An error occured bla bla (fill in later)
-        // error.code
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+            video: true 
+        }).then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        });
+    }
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 400, 300);
     });
 })();
